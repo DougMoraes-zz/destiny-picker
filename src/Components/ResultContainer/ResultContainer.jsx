43c747dcef9787de2506b07d4f6082abc7926b08
Douglas <dougmoraes91@gmail.com>
2020-06-04 17:56:26 +0200

    style: set minimal design for ResultContainer

--- src/Components/ResultContainer/ResultContainer.jsx
+++ src/Components/ResultContainer/ResultContainer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const ResultContainer = props => {
-  return <div>ResultContainer</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        border: "1px solid lightgray",
+        borderRadius: "10%",
+        minHeight: "10em",
+        margin: "5px"
+      }}
+    >
+      ResultContainer
+    </div>
+  );
 };
 
 export default ResultContainer;
