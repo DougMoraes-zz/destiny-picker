81217025b928b1d0584c6ae29fdd1f569e6cbad7
Douglas <dougmoraes91@gmail.com>
2020-06-05 23:30:09 +0200

    feat: create apiCall to get CityKey

--- src/Components/WeatherInfo/WeatherInfo.jsx
+++ src/Components/WeatherInfo/WeatherInfo.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import WeatherApi from "../../APIs/WeatherApi";
 
 const WeatherInfo = props => {
-  return <div>WeatherInfo</div>;
+  const [cityId, setCityId] = useState("");
+
+  useEffect(() => {
+    getCityId("amsterdam");
+  }, []);
+
+  const getCityId = cityName => {
+    WeatherApi.get(
+      `search?apikey=G5jhNzZpn0iWknOJM3pZzf4mrGFdzDzI&q=${cityName}`
+    )
+      .then(resp => resp.data[0].Key)
+      .then(key => setCityId(key))
+      .catch(err => console.log(err));
+  };
+
+  return <div>{cityId}</div>;
 };
 
 export default WeatherInfo;
