aca816746ec22969a39babe93389aa0b1a1cf61d
Douglas <dougmoraes91@gmail.com>
2020-06-06 23:21:44 +0200

    feat: add api call to get forecasts

--- src/Components/WeatherInfo/WeatherInfo.jsx
+++ src/Components/WeatherInfo/WeatherInfo.jsx
@@ -2,22 +2,29 @@ import React, { useState, useEffect } from "react";
 import WeatherApi from "../../APIs/WeatherApi";
 
 const WeatherInfo = props => {
-  const [cityId, setCityId] = useState("");
+  const [forecasts, setForecasts] = useState([]);
 
   useEffect(() => {
-    getCityId("amsterdam");
+    const getForecasts = cityKey => {
+      WeatherApi.forecasts
+        .get(`${cityKey}?apikey=G5jhNzZpn0iWknOJM3pZzf4mrGFdzDzI`)
+        .then(resp => resp.data.DailyForecasts)
+        .then(forecasts => setForecasts(forecasts))
+        .catch(err => console.log(err));
+    };
+
+    const getCityId = cityName => {
+      WeatherApi.locations
+        .get(`search?apikey=G5jhNzZpn0iWknOJM3pZzf4mrGFdzDzI&q=${cityName}`)
+        .then(resp => resp.data[0].Key)
+        .then(key => getForecasts(key))
+        .catch(err => console.log(err));
+    };
+
+    getCityId(props.cityName);
   }, []);
 
-  const getCityId = cityName => {
-    WeatherApi.get(
-      `search?apikey=G5jhNzZpn0iWknOJM3pZzf4mrGFdzDzI&q=${cityName}`
-    )
-      .then(resp => resp.data[0].Key)
-      .then(key => setCityId(key))
-      .catch(err => console.log(err));
-  };
-
-  return <div>{cityId}</div>;
+  return <div>WeatherInfo</div>;
 };
 
 export default WeatherInfo;
