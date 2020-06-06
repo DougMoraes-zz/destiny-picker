bbf4c1e78c46a163f1fed4c65a9f8ea8de9412fc
Douglas <dougmoraes91@gmail.com>
2020-06-06 23:13:23 +0200

    feat: add forecasts api base axios object

--- src/APIs/WeatherApi.js
+++ src/APIs/WeatherApi.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-const WeatherApi = axios.create({
-  baseURL: "http://dataservice.accuweather.com/locations/v1/cities/"
-});
+const WeatherApi = {
+  locations: axios.create({
+    baseURL: "http://dataservice.accuweather.com/locations/v1/cities/"
+  }),
+  forecasts: axios.create({
+    baseURL: "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
+  })
+};
 
 export default WeatherApi;
