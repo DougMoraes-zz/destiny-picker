2ff1bc26961773fabab43a6e1f84e4b42e872968
Douglas <dougmoraes91@gmail.com>
2020-06-07 01:34:12 +0200

    feat: add getFlights api call

--- src/Components/FlightInfo/FlightInfo.jsx
+++ src/Components/FlightInfo/FlightInfo.jsx
@@ -4,11 +4,20 @@ import FlightsApi from "../../APIs/FlightsApi";
 const FlightInfo = props => {
   const [idToCity, setIdToCity] = useState([]);
   const [idFromCity, setIdFromCity] = useState([]);
+  const [flights, setFlights] = useState([]);
 
   useEffect(() => {
     const getCityId = (cityName, updateState) => {
-      FlightsApi.get(`/locations?term=${cityName}`)
+      FlightsApi.get(`locations?term=${cityName}`)
         .then(resp => updateState(resp.data.locations[0].id))
+        .catch(err => console.log(err));
+    };
+
+    const getFlights = (fromCity, toCity, fromDate, toDate) => {
+      FlightsApi.get(
+        `flights?partner=picky&fly_from=${fromCity}&fly_to=${toCity}&date_from=${fromDate}&date_to=${toDate}`
+      )
+        .then(resp => setFlights(resp.data.data))
         .catch(err => console.log(err));
     };
 
