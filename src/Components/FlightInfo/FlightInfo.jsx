6756b44a75acd6c3685a3ce8d00802d995de55ef
Douglas <dougmoraes91@gmail.com>
2020-06-07 01:42:04 +0200

    feat: add CalculateAveragePrice function

--- src/Components/FlightInfo/FlightInfo.jsx
+++ src/Components/FlightInfo/FlightInfo.jsx
@@ -15,10 +15,22 @@ const FlightInfo = props => {
 
     const getFlights = (fromCity, toCity, fromDate, toDate) => {
       FlightsApi.get(
-        `flights?partner=picky&fly_from=${fromCity}&fly_to=${toCity}&date_from=${fromDate}&date_to=${toDate}`
+        `flights?partner=picky&max_stopovers=0&fly_from=${fromCity}&fly_to=${toCity}&date_from=${fromDate}&date_to=${toDate}`
       )
         .then(resp => setFlights(resp.data.data))
         .catch(err => console.log(err));
+    };
+
+    const CalculateAveragePrice = list => {
+      const sumPrices = list.reduce((result, item) => {
+        result += item.price;
+
+        return result;
+      }, 0);
+
+      const averagePrice = sumPrices / list.length;
+
+      return averagePrice;
     };
 
     getCityId(props.toCity, setIdToCity);
