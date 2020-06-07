2aaa223119759b321d78d787bc1153473ff7ea0a
Douglas <dougmoraes91@gmail.com>
2020-06-07 14:02:33 +0200

    feat: pass currentCityName to result components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ResultContainer from "./ResultContainer/ResultContainer";
-import { TextField } from "@material-ui/core";
+import { TextField, Button } from "@material-ui/core";
 
-function App() {
+const App = props => {
+  const [tempCityName, setTempCityName] = useState("");
+  const [currentCityName, setCurrentCityName] = useState("");
+
   return (
     <div
       style={{
@@ -12,12 +15,22 @@ function App() {
         alignItems: "center"
       }}
     >
-      <TextField label="Type your current city name..." />
-      <ResultContainer />
-      <ResultContainer />
-      <ResultContainer />
+      <TextField
+        label="Type your current city name..."
+        onChange={e => setTempCityName(e.currentTarget.value)}
+      />
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => setCurrentCityName(tempCityName)}
+      >
+        Let's start!
+      </Button>
+      <ResultContainer currentCity={currentCityName} />
+      <ResultContainer currentCity={currentCityName} />
+      <ResultContainer currentCity={currentCityName} />
     </div>
   );
-}
+};
 
 export default App;
