1ece7aa01e2dbfdd8644df5f95379b518fd17987
Douglas <dougmoraes91@gmail.com>
2020-06-07 13:23:27 +0200

    feat: add function to calculate average price

--- src/Components/FlightInfo/FlightInfo.jsx
+++ src/Components/FlightInfo/FlightInfo.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Typography } from "@material-ui/core";
 import FlightsApi from "../../APIs/FlightsApi";
 
 const FlightInfo = props => {
-  const [idToCity, setIdToCity] = useState([]);
-  const [idFromCity, setIdFromCity] = useState([]);
+  const [idToCity, setIdToCity] = useState("");
+  const [idFromCity, setIdFromCity] = useState("");
   const [flights, setFlights] = useState([]);
+  const [avgPrice, setAvgPrice] = useState(0);
 
   useEffect(() => {
     const getCityId = (cityName, updateState) => {
@@ -17,30 +19,36 @@ const FlightInfo = props => {
       FlightsApi.get(
         `flights?partner=picky&max_stopovers=0&fly_from=${fromCity}&fly_to=${toCity}&date_from=${fromDate}&date_to=${toDate}`
       )
-        .then(resp => setFlights(resp.data.data))
+        .then(resp => {
+          setFlights(resp.data.data);
+          CalculateAveragePrice(resp.data.data);
+        })
         .catch(err => console.log(err));
     };
 
     const CalculateAveragePrice = list => {
       const sumPrices = list.reduce((result, item) => {
-        result += item.price;
-
-        return result;
+        return (result += parseFloat(item.price));
       }, 0);
 
       const averagePrice = sumPrices / list.length;
+
+      setAvgPrice(averagePrice);
 
       return averagePrice;
     };
 
     getCityId(props.toCity, setIdToCity);
     getCityId(props.fromCity, setIdFromCity);
+
+    getFlights(idFromCity, idToCity, props.fromDate, props.toDate);
   }, [props.toCity, props.fromCity]);
 
-  console.log(idToCity);
-  console.log(idFromCity);
-
-  return <div>FlightInfo</div>;
+  return (
+    <div>
+      <Typography>{`Average Price: ${avgPrice} `}</Typography>
+    </div>
+  );
 };
 
 export default FlightInfo;
